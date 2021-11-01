79a2450cc9c3f55a32c0c07b1cea58f3a48c5162
adamarsenault <adam.j.arsenault@gmail.com>
2021-11-01 14:47:28 -0400

    restructure / errors

--- powershell/win-desktop-automation/desktop-machine-setup.ps1
+++ powershell/win-desktop-automation/desktop-machine-setup.ps1
@@ -1,25 +1,27 @@
 $appspath = $PSScriptRoot + '\' + 'desktop-machine-apps.txt'
 $apps = Get-Content -Path $appspath
 
-if (-not (choco config get cacheLocation))
-{
+try {
+    choco config get cacheLocation
+} catch {
     Write-Host "Chocolatey not detected, attempting to install now"
     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-}
-else
-{
-    Write-Host "Chocolatey dectected, attempting to install applications from config file."
 }
 
 if ((choco config get cacheLocation))
 {
     try {  
+        Write-Host "Chocolatey dectected, attempting to install applications from config file."
         foreach ($app in $apps)
-        {
-            Write-Host "Installing $app"
-            & choco install $app /y  | Write-Host
-        }
+            {
+                Write-Host "Installing $app"
+                & choco install $app /y  | Write-Host
+            }
     } catch {
         Write-Error -Message "$app failed to install."
     }
+}
+else
+{ 
+    Write-Error -Message "Machine setup did not run correctly." -RecommendedAction "Run script as elevated administrator and/or manually install chocolatey and try again."
 }
